31ec8afad6efef8a5e9556e33e0b1ec653230e74
Matheus Gustavo Rieper Matos de Oliveira <167656732+MatheusRieperdeOliveira@users.noreply.github.com>
2024-09-25 23:10:03 -0300

    Delete calculadora.js

--- calculadora.js
+++
@@ -1,9 +0,0 @@
-const corpo_calculadora = window.document.querySelector(".corpo_da_calculadora")
-const tela = window.document.getElementById('tela')
-const botao = window.document.getElementById('botoes')
-
-const cliquei = (numero) =>{
-    tela.innerText = numero
-}
-
-tela.addEventListener
